7518d400b00956bff183ec01c2611b287f382f9d
Tom <80857044+ike-tom@users.noreply.github.com>
2021-06-20 13:10:05 +0200

    adjustments in social media bar, script corrections

--- script/script.js
+++ script/script.js
@@ -1,9 +1,17 @@
 const btn = document.querySelector(".cta__button")
 const socialMediaBar = document.querySelector(".social-media__bar")
+const mainContainer = document.querySelector(".main__container")
 
 const makeVisible = function (arg) {
     if (arg.classList.contains("visible")) arg.classList.remove("visible")
     else arg.classList.add("visible")
 }
 
-btn.addEventListener("click", function () { makeVisible(socialMediaBar) })
+btn.addEventListener("click", function (e) {
+    e.stopPropagation()
+    makeVisible(socialMediaBar)
+})
+
+mainContainer.addEventListener('click', function () {
+    socialMediaBar.classList.remove("visible")
+})
